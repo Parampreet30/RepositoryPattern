7a44469efe721ec89efd2791f6c98dc108cb96b5
Parampreet30 <A00264414@mycambrian.ca>
2024-10-01 23:51:44 +0000

    blog.cs file created

---
+++ RepoPattern/Models/Blog.cs
@@ -0,0 +1,11 @@
+namespace RepositoryPattern.RepoPattern;
+
+public class Blog
+{
+    public string Name {get; set; }
+
+    public string Description {get; set;}
+
+    public string URL {get; set;}
+
+}
